4edfee964ae110a9144d3359ba1465aca2a3ecf9
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-01-07 10:49:08 -0500

    Wrote test and code to change input value

--- counter-testing/src/components/counter/Counter.js
+++ counter-testing/src/components/counter/Counter.js
@@ -1,7 +1,21 @@
+import { useState } from 'react'
+import './Counter.css'
 const Counter = () => {
+
+  const [count, setCount] = useState(0)
+  const [value, setValue] = useState(1)
+
+  const onValueChange = (e) => {
+    setValue(e.target.value)
+  }
+
   return (
     <div>
       <h1>Counter</h1>
+      <h2 data-testid='counter'>{count}</h2>
+      <button data-testid='subtract-button'>-</button>
+      <button data-testid='add-button'>+</button>
+      <input className='text-center' data-testid='input' type='number' value={value} onChange={onValueChange} />
     </div>
   )
 }
--- counter-testing/src/components/counter/Counter.test.js
+++ counter-testing/src/components/counter/Counter.test.js
@@ -1,8 +1,45 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Counter from './Counter'
 
 test('header renders with correct test', () => {
   render(<Counter />)
-  const head = screen.getByRole('heading')
-  expect(head.textContent).toBe("Counter")
+  const head = screen.getByRole('heading', { name: /Counter/i })
+  expect(head).toBeInTheDocument()
+})
+
+test('counter initally starts with text of 0', () => {
+  render(<Counter />)
+  const count = screen.getByTestId('counter')
+  expect(count.textContent).toEqual("0")
+})
+
+test('input contains iitial value of one', () => {
+  render(<Counter />)
+  const input = screen.getByTestId('input')
+  expect(input.value).toBe("1")
+})
+
+test('add button renders with +', () => {
+  render(<Counter />)
+  const btn = screen.getByTestId('add-button', { name: /add button/i })
+
+  expect(btn.textContent).toBe("+")
+})
+
+test('subtract button renders with -', () => {
+  render(<Counter />)
+  const btn = screen.getByTestId('subtract-button', { name: /subtract button/i })
+  expect(btn.textContent).toBe("-")
+})
+
+test('changing value of input works correctly', () => {
+  render(<Counter />)
+  const inputEle = screen.getByTestId('input')
+  expect(inputEle.value).toBe('1')
+  fireEvent.change(inputEle, {
+    target: {
+      value: '5'
+    }
+  })
+  expect(inputEle.value).toBe('5')
 })
